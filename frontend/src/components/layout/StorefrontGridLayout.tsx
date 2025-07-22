3f057602292d7161cfae4cba791486b12c742503
Concal <cpc329@gmail.com>
2025-07-22 00:36:21 -0500

    bring back single column

--- frontend/src/components/layout/StorefrontGridLayout.tsx
+++ frontend/src/components/layout/StorefrontGridLayout.tsx
@@ -11,7 +11,7 @@ export function StorefrontGridLayout({
 }: StorefrontGridLayoutProps) {
   return (
     <div
-      className={`grid gap-4 grid-cols-2 lg:grid-cols-3 xl:!grid-cols-4 ${className}`}
+      className={`grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:!grid-cols-4 ${className}`}
     >
       {children}
     </div>
